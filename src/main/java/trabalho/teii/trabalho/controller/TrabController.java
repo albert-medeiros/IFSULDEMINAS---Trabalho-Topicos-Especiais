1d810e4aff910aa838c2ebd254e16510066dd54b
Albert <albertmdepaula.com>
2021-06-25 23:49:38 -0300

    C3 - Realizando a configuração do ambiente e fazendo os primeiros testes

---
+++ src/main/java/trabalho/teii/trabalho/controller/TrabController.java
@@ -0,0 +1,14 @@
+package trabalho.teii.trabalho.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class TrabController {
+    @GetMapping("/trabalho")
+    public String hello(){
+        return "hello, World!";
+    }
+
+}
