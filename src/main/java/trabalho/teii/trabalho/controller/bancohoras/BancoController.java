01268a801647d7ab6747d7da9d62e7ee0d98f819
Rafael Vinicius <contato.rvproductions@gmail.com>
2021-07-18 21:55:16 -0300

    C20 - Criando Método Show

--- src/main/java/trabalho/teii/trabalho/controller/bancohoras/BancoController.java
+++ src/main/java/trabalho/teii/trabalho/controller/bancohoras/BancoController.java
@@ -1,6 +1,7 @@
 package trabalho.teii.trabalho.controller.bancohoras;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import trabalho.teii.trabalho.model.entity.Usuario;
@@ -13,6 +14,7 @@ import java.util.List;
 public class BancoController {
     //CRUDs
 
+    //READ
     //Método List
     private BancoRepository bancoRepository;
 
@@ -22,5 +24,11 @@ public class BancoController {
     @GetMapping
     public List<Usuario> list(){
         return bancoRepository.findAll();
+    }
+
+    //Método Show
+    @GetMapping("/{id}")
+    public Usuario show(@PathVariable Long id){
+        return bancoRepository.getById(id);
     }
 }
