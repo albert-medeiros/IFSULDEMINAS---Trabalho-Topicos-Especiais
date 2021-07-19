085a4b428468b18a0a49978a45b56f85d4fb4da0
Rafael Vinicius <contato.rvproductions@gmail.com>
2021-07-18 21:49:56 -0300

    C19 - Criando Método List

--- src/main/java/trabalho/teii/trabalho/controller/bancohoras/BancoController.java
+++ src/main/java/trabalho/teii/trabalho/controller/bancohoras/BancoController.java
@@ -1,8 +1,26 @@
 package trabalho.teii.trabalho.controller.bancohoras;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import trabalho.teii.trabalho.model.entity.Usuario;
+import trabalho.teii.trabalho.model.repositories.BancoRepository;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/usuarios")
 public class BancoController {
     //CRUDs
+
+    //Método List
+    private BancoRepository bancoRepository;
+
+    public BancoController(BancoRepository bancoRepository){
+        this.bancoRepository = bancoRepository;
+    }
+    @GetMapping
+    public List<Usuario> list(){
+        return bancoRepository.findAll();
+    }
 }
